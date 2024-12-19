06e805052559e966d71c93c43f0d2f75116942b9
XavierMerino01 <71768212+XavierMerino01@users.noreply.github.com>
2024-12-19 18:11:25 +0100

    All screens final set-up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './styles.css';
 
 const codis = [
-    { code: "Tetitas" },
-    { code: "Huevazos" }
+    { code: "Tetitas", state: false },
+    { code: "Huevazos", state: false }
 ];
 
 let codeCounter = 0;
 
 const secondBg = document.querySelector(".second");
 const thirdBg = document.querySelector(".third");
-const fourthBg = document.querySelector(".fourth");
+const poster = document.querySelector(".poster");
 
 document.addEventListener('DOMContentLoaded', () => {
     const codeInput = document.querySelector(".code-input");
@@ -17,29 +17,40 @@ document.addEventListener('DOMContentLoaded', () => {
     codeInput.addEventListener('input', () => {
         checkInputValue(codeInput.value);
     });
+
+    const returnButton = document.querySelector(".button-55");
+
+    returnButton.addEventListener('click', ()=>{
+        onComplete(1);
+    });
 });
 
 function checkInputValue(code) {
     const capitalizedCode = capitalize(code);
     const codeInput = document.querySelector(".code-input");
 
-    if (capitalizedCode === codis[0].code) {
+    if (capitalizedCode === codis[0].code && codis[0].state === false ) {
         codeCounter++;
+        codis[0].state = true;
         if (codeCounter === 2) {
             onComplete(4);
         } else {
             onComplete(2)
         }
         codeInput.value = "";
-    } else if (capitalizedCode === codis[1].code) {
+        codeInput.blur();
+    } else if (capitalizedCode === codis[1].code && codis[1].state === false) {
         codeCounter++;
+        codis[1].state = true;
         if (codeCounter === 2) {
             onComplete(4);
         } else {
             onComplete(3);
         }
         codeInput.value = "";
+        codeInput.blur();
     }
+
 
     console.log(capitalizedCode+" : "+ code);
 }
@@ -50,16 +61,18 @@ function onComplete(bgIndex) {
         case 1 :
             secondBg.classList.remove('visible');
             thirdBg.classList.remove('visible');
-            fourthBg.classList.remove('visible');
             break;
         case 2 :
             secondBg.classList.add('visible');
+            poster.classList.add('1');
             break;
         case 3 :
-            thirdBg.classList.add('visible');
+            secondBg.classList.add('visible');
+            poster.classList.add('2');
             break;
         case 4 :
-            fourthBg.classList.add('visible');
+            secondBg.classList.remove('visible');
+            thirdBg.classList.add('visible');
             break;
         
     }
