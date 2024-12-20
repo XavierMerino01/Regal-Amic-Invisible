47f47bcfaf8d5ee6148880c55255a7bf7efe39e4
XavierMerino01 <merinoxevi@gmail.com>
2024-12-20 16:14:54 +0100

    Distribution Changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,4 +26,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
 };
